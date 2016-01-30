18d15b825d9f612f6447340a6b65a3cc67249558
Kayra <KayraAlat@gmail.com>
2016-01-30 23:10:48 +0000

    Added dictionary to object subclasses

---
+++ dict_to_obj.py
@@ -0,0 +1,23 @@
+
+# Just for better way to reference
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
+
+# With dict methods
+class objdict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
