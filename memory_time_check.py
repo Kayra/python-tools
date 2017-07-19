e3402901411fdf446c8c05d4706200b204ae57a3
Kayra <KayraAlat@gmail.com>
2017-07-19 11:43:00 +0100

    Updated memory checking

--- memory_time_check.py
+++ memory_time_check.py
@@ -10,18 +10,23 @@ def function_two():
     pass
 
 
-print('Memory BEFORE: {}Mb'.format(mem_profile.memory_usage_resource()))
-
+memory_start_one = mem_profile.memory_usage_resource()
 timer_start_one = time.clock()
 function_one()
 timer_end_one = time.clock()
+memory_end_one = mem_profile.memory_usage_resource()
 
+
+memory_start_two = mem_profile.memory_usage_resource()
 timer_start_two = time.clock()
 function_two()
 timer_end_two = time.clock()
-
-print('Memory AFTER: {}Mb'.format(mem_profile.memory_usage_resource()))
+memory_end_two = mem_profile.memory_usage_resource()
 
 
 print('The first function took {} seconds'.format(timer_end_one - timer_start_one))
 print('The second function took {} seconds'.format(timer_end_two - timer_start_two))
+
+
+print('The first function used {}mb'.format(memory_end_one - memory_start_one))
+print('The second function used {}mb'.format(memory_end_two - memory_start_two))
